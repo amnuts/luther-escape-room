d01139fd722daea3e20d532c1b530cab844ceb4c
Andrew Collington <andy@amnuts.com>
2019-06-22 20:14:48 +0100

    Refactoring of control code.
    Have moved configuration to a separate file and made the setup of the buttons more dynamic based on the config.
    
    The socket setup in the index.js file has also been refactored to cut down on the amount of code due to the nature of the events being very similar.

--- index.js
+++ index.js
@@ -10,30 +10,13 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 app.get('/control', (req, res) => res.sendFile(__dirname + '/control.html'));
 
 io.on('connection', (socket) => {
-    socket.on('video-start', function(data){
-        socket.broadcast.emit('video-start', data);
-    });
-    socket.on('video-loaded', function(data){
-        socket.broadcast.emit('video-loaded', data);
-    });
-    socket.on('video-playing', function(data){
-        socket.broadcast.emit('video-playing', data);
-    });
-    socket.on('video-ended', function(data){
-        socket.broadcast.emit('video-ended', data);
-    });
-
-    socket.on('audio-start', function(data){
-        socket.broadcast.emit('audio-start', data);
-    });
-    socket.on('audio-loaded', function(data){
-        socket.broadcast.emit('audio-loaded', data);
-    });
-    socket.on('audio-playing', function(data){
-        socket.broadcast.emit('audio-playing', data);
-    });
-    socket.on('audio-ended', function(data){
-        socket.broadcast.emit('audio-ended', data);
+    ['start', 'loaded', 'playing', 'ended'].forEach(function(element) {
+        socket.on(`video-${element}`, function(data){
+            socket.broadcast.emit(`video-${element}`, data);
+        });
+        socket.on(`audio-${element}`, function(data){
+            socket.broadcast.emit(`audio-${element}`, data);
+        });
     });
 });
 
---
+++ public/config.js
@@ -0,0 +1,44 @@
+const config = {
+    ticking: {
+        max: 7,
+        min: 1
+    },
+    success: {
+        video: "winner_winner_chicken_dinner",
+        format: "mp4"
+    },
+    failure: {
+        audio: "buzzer",
+        format: "mp3"
+    },
+    buttons: {
+        video: {
+            intro: {
+                text: "Introduction",
+                format: "mp4"
+            },
+            did_stuff: {
+                text: "People did stuff",
+                format: "mp4"
+            },
+            try_harder: {
+                text: "Try harden!",
+                format: "mp4"
+            }
+        },
+        audio: {
+            attic_door: {
+                text: "Creepy attic door creaking",
+                format: "mp3"
+            },
+            creepy_child: {
+                text: "Creepy ghost child",
+                format: "mp3"
+            },
+            monster_echo: {
+                text: "Monster's growl",
+                format: "mp3"
+            }
+        }
+    }
+};
