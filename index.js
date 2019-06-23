64f450d5f8a19906bf87544477545d96dda1716c
Andrew Collington <andy@amnuts.com>
2019-06-23 02:09:18 +0100

    Added incantation input.
    Check was put onto the express server so that the password could be tucked away from the view-source (kids might be sneaky, I don't know!).  Will react depending on if the input is successful or not.
    
    Yes, the incantation is in the source code.  No, I really don't care (especially at 2am!)

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const express = require('express');
 const app = express();
 const http = require("http").createServer(app);
 const io = require('socket.io')(http);
+const bodyParser  = require('body-parser');
 const port = 3000;
+// kid's never see this, so just putting here for now
+const incantation = 'spooky';
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 app.get('/game', (req, res) => res.sendFile(__dirname + '/game.html'));
 app.get('/control', (req, res) => res.sendFile(__dirname + '/control.html'));
+app.post('/check', (req, res) => {
+    if (req.body.incantation.toUpperCase() === incantation.toUpperCase()) {
+        res.status(200);
+        res.send('Congratulations!');
+    } else {
+        res.status(403);
+        res.send('Oh no!');
+    }
+});
 
 io.on('connection', (socket) => {
     ['start', 'loaded', 'playing', 'ended'].forEach(function(element) {
